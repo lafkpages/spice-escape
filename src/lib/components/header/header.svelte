1824b209d5f5e2e15b29ba650702565eab96cab7
lafkpages <luisafk@proton.me>
2023-11-14 16:33:58 +0100

    Header animation

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -1,4 +1,14 @@
-<header>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let anim = false;
+
+	onMount(() => {
+		anim = true;
+	});
+</script>
+
+<header class:anim>
 	<img src="/img/headerNoHotSauce.png" alt="Spice Escape" class="header" />
 	<img src="/img/hotSauce.png" alt="" class="hot-sauce" />
 </header>
@@ -13,6 +23,14 @@
 		justify-content: center;
 
 		position: relative;
+
+		animation-duration: 1s;
+		animation-timing-function: ease-out;
+		animation-fill-mode: forwards;
+
+		&.anim {
+			animation-name: slidein;
+		}
 
 		img {
 			image-rendering: crisp-edges;
@@ -29,6 +47,16 @@
 			top: 19%;
 			height: 62.2%;
 			transform: rotate(-15deg);
+		}
+	}
+
+	@keyframes slidein {
+		from {
+			transform: translateX(-125vw);
+		}
+
+		to {
+			transform: translateX(0px);
 		}
 	}
 </style>
